b8f1795499a9799ee3f79cc13b94c89aa546da94
iamdevgeek <iamdevgeek@gmail.com>
2021-01-14 15:18:13 -0800

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/swetang/final_project_ws/src
+# Build directory: /home/swetang/final_project_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_navigation")
+subdirs("my_simulation")
+subdirs("turtlebot3_gazebo")
+subdirs("turtlebot3_slam")
+subdirs("turtlebot3_teleop")
+subdirs("turtlebot3_description")
